2eec5f067c2355cdb233b0defd0a9bd23494a1fc
dankwon234 <dan.kwon234@gmail.com>
2016-03-30 15:14:19 -0400

    gulp tasks

--- public/app/components/containers/Upload.js
+++ public/app/components/containers/Upload.js
@@ -4,14 +4,7 @@ var Upload = React.createClass({
 
 	render: function(){
 		return (
-			<div className="container">
-				<h1>Upload</h1>
-				<form action="/upload/test" method="post" encType="multipart/form-data">
-				    <p>Public ID: <input type="text" name="title"/></p>
-				    <p>Image: <input type="file" name="image"/></p>
-				   	<p><input type="submit" value="Upload"/></p>
-				</form>
-
+			<div className="container">Upload PAGE
 			</div>
 		);
 	}
--- routes/upload.js
+++ routes/upload.js
@@ -1,29 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-var fs = require('fs');
-var cloudinary = require('cloudinary');
-cloudinary.config({ 
-  cloud_name: 'sample', 
-  api_key: '874837483274837', 
-  api_secret: 'a676b67565c6767a6767d6767f676fe1' 
-});
-
-// var stream = cloudinary.uploader.upload_stream(function(result) { console.log(result); });
-// var file_reader = fs.createReadStream('my_picture.jpg', {encoding: 'binary'}).on('data', stream.write).on('end', stream.end);
-
 router.post('/:resource', function(req, res, next) {
-//    res.render('index', { title: 'Express' });
 
-	var stream = cloudinary.uploader.upload_stream(function(result) {
-	    console.log('RESULT: '+result);
+    username = req.body.username;
+    full_name = req.body.full_name;
+    avatar_url = req.body.avatar_url;
+    update_account(username, full_name, avatar_url); // TODO: create this function
+    // TODO: Return something useful or redirect
 
-	    res.send('Done:<br/> <img src="' + result.url + '"/><br/>' +
-	             cloudinary.image(result.public_id, { format: "png", width: 100, height: 130, crop: "fill" }));
-	  }, 
-	  { public_id: req.body.title } );
-	
-	  fs.createReadStream(req.files.image.path, {encoding: 'binary'}).on('data', stream.write).on('end', stream.end);
+    res.json({
+    	username: username,
+    	full_name: full_name,
+    	avatar_url: avatar_url
+    });
 
 });
 
